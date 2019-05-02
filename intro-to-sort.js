1fe953b40b523c10528cb84ca833cdb11a735e6f
venetrius <gergelygjuhasz@gmail.com>
2019-05-02 19:07:05 +0000

    init repo / add assignment description

---
+++ intro-to-sort.js
@@ -0,0 +1,13 @@
+// solution for https://web.compass.lighthouselabs.ca/days/w01d4/activities/188
+
+/* init data */
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+/* spec : Write a sorting function (sometimes called a custom comparator),
+ that sorts the above array first by the name ascending alphabetically, and in cases where the names are equal sort by descending age. */
